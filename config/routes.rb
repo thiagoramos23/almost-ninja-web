9407c71d4e7c6d7086ca778adbc839e9a02815a9
Thiago Ramos <thiagoramos.al@gmail.com>
2020-05-02 00:30:01 -0300

    Adjusted the routes to work with the new namespaces for the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :account, controllers: { sessions: 'account/sessions', registrations: 'account/registrations' }
-  root 'courses#index'
+  root 'course/courses#index'
 
-  scope module: :courses, path: '/' do
+  scope module: 'course' do
+    get 'my-courses', to: 'courses#my_courses'
+
     resources :courses, only: [:index, :show]
-    get 'my-courses', action: 'my_courses'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
