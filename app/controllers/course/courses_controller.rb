cdfa0467125c0fb893b1fa4691a7f126a4e36e31
Thiago Ramos <thiagoramos.al@gmail.com>
2020-05-03 00:58:13 -0300

    Changed call to repository methods instead of call the active record methods directly

--- app/controllers/course/courses_controller.rb
+++ app/controllers/course/courses_controller.rb
@@ -1,14 +1,13 @@
 class Course::CoursesController < ApplicationController
-  before_action :authenticate_account!
+  before_action :authenticate_account!, only: [:my_courses]
 
   def index
-    @courses = ::Course::Course.includes(:tags).all
+    @courses = ::Course::Course.all_available_courses
     render 'courses/index'
   end
 
   def my_courses
-    # @courses = ::Course::Course.user_registered_courses(current_account)
-    @courses = current_account.user.courses.includes(:tags).all
+    @courses = ::Course::Course.user_registered_courses(current_account)
     render 'courses/index'
   end
 
