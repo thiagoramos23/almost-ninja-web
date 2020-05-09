15b5369cb7db45a82bb1e6565ee1a6a13de3552c
Thiago Ramos <thiagoramos.al@gmail.com>
2020-05-08 21:47:02 -0300

    Added locale and set default to PT-br

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module AlmostNinjaWeb
     config.load_defaults 6.0
     config.generators.assets = false
     config.generators.helper = false
+
+    config.i18n.default_locale = 'pt-BR'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
