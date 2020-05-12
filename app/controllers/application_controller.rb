8cbe627410735e866a004db4517275f6f73961ef
Thiago Ramos <thiagoramos.al@gmail.com>
2020-05-11 23:51:42 -0300

    Added authenticate_account to redirecto to login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  protected
+
+  def authenticate_account!
+    if account_signed_in?
+      super
+    else
+      redirect_to new_account_session_url
+    end
+  end
 end
