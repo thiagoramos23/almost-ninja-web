6635a2bce0cd1b92834949cbae88aacdd629ec22
Thiago Ramos <thiagoramos.al@gmail.com>
2020-05-20 01:26:06 -0300

    Added aspect ration library for tailwind. Solved the problems for the design of the screen. Finally being able to make some logic tomorrow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 module.exports = {
-  theme: {
-    fontFamily: {
-      body: ['Lato', 'sans-serif'],
-      display: ['Lato', 'sans-serif'],
+    theme: {
+        fontFamily: {
+            body: ['Lato', 'sans-serif'],
+            display: ['Lato', 'sans-serif'],
+        },
+        aspectRatio: { // defaults to {}
+            'none': 0,
+            'square': [1, 1], // or 1 / 1, or simply 1
+            '16/9': [16, 9], // or 16 / 9
+            '4/3': [4, 3], // or 4 / 3
+            '21/9': [21, 9], // or 21 / 9
+        },
+        extend: {
+            spacing: {
+                '96': '24rem',
+                '128': '32rem',
+                '160': '40rem',
+                '192': '48rem',
+            }
+        },
     },
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    variants: {
+        aspectRatio: ['responsive'], // defaults to ['responsive']
+    },
+    plugins: [
+        require('tailwindcss-aspect-ratio'),
+    ],
 }
