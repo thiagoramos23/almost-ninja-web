020abe8e1a6c84177381e1481b6a7bae84dd2aac
Thiago Ramos <thiagoramos.al@gmail.com>
2020-05-25 23:52:53 -0300

    Added specs for lectures controller.

--- app/controllers/course/lectures_controller.rb
+++ app/controllers/course/lectures_controller.rb
@@ -2,18 +2,29 @@ module Course
   class LecturesController < ApplicationController
     before_action :authenticate_account!
     before_action :set_course
+    before_action :set_lecture, only: [:show]
 
     def index
-      render 'lectures/index'
+      redirect_to course_lecture_url(@course, first_lecture)
     end
 
     def show
+      @lectures = ::Course::Lecture.all_course_lectures(@course.id)
+      render 'lectures/show'
     end
 
     private
 
     def set_course
       @course = ::Course::Course.find(params[:course_id])
+    end
+
+    def set_lecture
+      @lecture = ::Course::Lecture.find(params[:id])
+    end
+
+    def first_lecture
+      ::Course::Lecture.first_course_lecture(@course.id)
     end
   end
 end
--- spec/controllers/course/lectures_controller_spec.rb
+++ spec/controllers/course/lectures_controller_spec.rb
@@ -1,16 +1,35 @@
-module Course
-  class LecturesController < ApplicationController
-    before_action :authenticate_account!
+require 'rails_helper'
 
-    def index
+describe ::Course::LecturesController, type: %i[controller course] do
+  login_account
+  let!(:course)         { Fabricate :course }
+  let!(:lecture_module) { Fabricate :lecture, module: nil, course: course }
+  let!(:lecture)        { Fabricate :lecture, module: lecture_module, course: course }
+  let!(:second_lecture) { Fabricate :lecture, module: lecture_module, course: course }
+
+  describe 'GET index' do
+    before do
+      get :index, params: { course_id: course.id }
     end
 
-    def show
+    it 'should redirect to show' do
+      expect(response).to redirect_to(course_lecture_path(course, lecture))
+    end
+  end
+
+  describe 'GET show' do
+    before do
+      get :show, params: { course_id: course.id, id: lecture.id }
     end
 
-    private
+    it 'should assign lecture and lectures variables' do
+      expect(assigns(:lecture)).to eq lecture
+      expect(assigns(:lectures)).to eq [lecture, second_lecture]
+    end
 
-    def set_lecture
+    it 'should render lectures/show' do
+      expect(response).to render_template('lectures/show')
+      expect(response).to have_http_status(:ok)
     end
   end
 end
